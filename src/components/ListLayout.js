54d0a94f936630f8d01250b2149c6952ecb3f523
김익현 <kimikhyun@ikkim01-MacBookAir.local>
2022-08-24 17:12:58 +0900

    Add:쿼리문 추가

--- src/components/ListLayout.js
+++ src/components/ListLayout.js
@@ -1,33 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./ListLayout.scss";
 
 const ListLayout = ({ dataBase }) => {
-  console.log(dataBase);
+  const navigate = useNavigate();
   return (
     <div className="box">
-      {dataBase.map((data) => {
-        const {
-          Name,
-          Actor,
-          Age,
-          Episode,
-          Genre,
-          Image,
-          Release,
-          SubTitle,
-          Top10,
-          Director,
-          Minute,
-        } = data;
-        const date = new Date(Release).getFullYear();
+      {dataBase.map((data, idx) => {
+        const { Name, Image, Top10, Director } = data;
+
         return (
-          <div className="content">
-            <h1>{Name}</h1>
-            {Director && <p>감독 : {Director}</p>}
-            <p>출연 : {Actor.map((actor) => `${actor} `)}</p>
-            <p>시청 연령 : {Age} 세이상 시청</p>
-            {Episode && <p>에피소드 {Episode}개</p>}
-            <p>장르 : {Genre.map((genre) => `${genre} `)}</p>
+          <div className="content" key={idx}>
             <div className="imgbox">
               <img src={`${Image}`} alt={`${Name}`} />
               {Top10 === true && (
@@ -38,9 +21,6 @@ const ListLayout = ({ dataBase }) => {
                 </p>
               )}
             </div>
-            {Minute && <p>상영시간 : {Minute}분</p>}
-            <p>{date}</p>
-            <p>{SubTitle}</p>
           </div>
         );
       })}
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,42 +1,77 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy } from "react";
 import { firestore } from "../../api/firebase";
 import ListLayout from "../../components/ListLayout";
+import "./main.scss";
 
 const Main = () => {
   const [dramaDatabase, setDramaDateBase] = useState([]);
   const [movieDatabase, setMovieDataBase] = useState([]);
 
+  const [dramaFilter, setDramaFilter] = useState("");
+  const [movieFilter, setMovieFilter] = useState("");
+
   useEffect(() => {
     setDramaDateBase([]);
     const koreanDrama = firestore.collection("Korean Drama");
-
-    koreanDrama.get().then((docs) => {
-      docs.forEach((doc) =>
-        setDramaDateBase((drama) => [...drama, doc.data()])
-      );
-    });
-  }, []);
+    dramaFilter
+      ? koreanDrama
+          .where("Genre", "array-contains", `${dramaFilter}`)
+          .get()
+          .then((docs) => {
+            docs.forEach((doc) =>
+              setDramaDateBase((drama) => [...drama, doc.data()])
+            );
+          })
+      : koreanDrama.get().then((docs) => {
+          docs.forEach((doc) =>
+            setDramaDateBase((drama) => [...drama, doc.data()])
+          );
+        });
+  }, [dramaFilter]);
 
   useEffect(() => {
     setMovieDataBase([]);
-    const koreanDrama = firestore.collection("HollyWoodMovie");
+    const HollyWoodMovie = firestore.collection("HollyWoodMovie");
+    movieFilter
+      ? HollyWoodMovie.where("Genre", "array-contains", `${movieFilter}`)
+          .get()
+          .then((docs) => {
+            docs.forEach((doc) =>
+              setMovieDataBase((movie) => [...movie, doc.data()])
+            );
+          })
+      : HollyWoodMovie.get().then((docs) => {
+          docs.forEach((doc) =>
+            setMovieDataBase((movie) => [...movie, doc.data()])
+          );
+        });
+  }, [movieFilter]);
 
-    koreanDrama.get().then((docs) => {
-      docs.forEach((doc) =>
-        setMovieDataBase((movie) => [...movie, doc.data()])
-      );
-    });
-  }, []);
-
-  if (dramaDatabase.length === 0) return <>Loading...</>;
-
-  console.log(dramaDatabase);
-  console.log(movieDatabase);
   return (
-    <div>
-      <ListLayout dataBase={dramaDatabase} />
-      <ListLayout dataBase={movieDatabase} />
-    </div>
+    <>
+      <div className="header">
+        <h1>한국 드라마{dramaFilter && `중 ${dramaFilter}`}</h1>
+        <div className="button-box">
+          <button onClick={() => setDramaFilter("")}>전체보기</button>
+          <button onClick={() => setDramaFilter("스릴러 시리즈")}>
+            스릴러 시리즈
+          </button>
+          <button onClick={() => setDramaFilter("범죄 시리즈")}>
+            범죄 시리즈
+          </button>
+        </div>
+        <ListLayout dataBase={dramaDatabase} />
+      </div>
+      <div className="header">
+        <h1>할리우드 영화{movieFilter && `중 ${movieFilter}`}</h1>
+        <div className="button-box">
+          <button onClick={() => setMovieFilter("")}>전체보기</button>
+          <button onClick={() => setMovieFilter("SF 영화")}>SF 영화</button>
+          <button onClick={() => setMovieFilter("호러 영화")}>호러 영화</button>
+        </div>
+        <ListLayout dataBase={movieDatabase} />
+      </div>
+    </>
   );
 };
 
